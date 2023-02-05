65455ba3ac66f56f4f89b7b4edd9072694f4f075
memo <alexis45645@gmail.com>
2023-02-05 07:23:20 -0500

    Updated to hashmaps

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
@@ -8,17 +7,17 @@ public class Solution {
 		System.out.println(lengthOfLongestSubstring("abcabcbb"));
 	}
     public static int lengthOfLongestSubstring(String s) {
-        Set<Character> charSet = new HashSet<>();
+        Map<Character, Integer> charMap = new HashMap<>();
         int n = s.length();
-        int maxLen = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            if (!charSet.contains(s.charAt(j))) {
-                charSet.add(s.charAt(j++));
-                maxLen = Math.max(maxLen, j - i);
-            } else {
-                charSet.remove(s.charAt(i++));
+        int maxLen = 0, i = 0;
+        for (int j = 0; j < n; j++) {
+            if (charMap.containsKey(s.charAt(j))) {
+                i = Math.max(i, charMap.get(s.charAt(j)) + 1);
             }
+            charMap.put(s.charAt(j), j);
+            maxLen = Math.max(maxLen, j - i + 1);
         }
         return maxLen;
     }
 }
+
